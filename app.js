892da2cf2458842d3f4545751ac95c2dedaa8a0a
theTechSurya <surajsutar467@gmail.com>
2021-06-29 18:49:43 +0530

    Startup Website Pro 1.0

---
+++ app.js
@@ -0,0 +1,24 @@
+// Scroll toP
+const scrolltp = document.querySelector("#scrolltp");
+scrolltp.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+window.addEventListener('scroll', function() {
+    if (window.scrollY >= 700) {
+        scrolltp.style.opacity = 1;
+    } else {
+        scrolltp.style.opacity = 0;
+    }
+});
+
+// Theme
+const themeToggle = document.querySelector(".checkbox");
+const body = document.querySelector("body");
+
+themeToggle.addEventListener("change", function() {
+    body.classList.toggle("dark")
+})
